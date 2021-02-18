212211e117151662ab99643ae0f438eb7f11963f
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-02-19 00:27:16 +0100

    commit

--- search-and-log.vb
+++ search-and-log.vb
@@ -1,58 +1,92 @@
 Option Explicit
-Const A_colRef As Integer = 3 
-Const A_colLnk As Integer = 6   
-Const B_colRef As Integer = 3 
-Const B_colLnk As Integer = 6
 
-Const A_SheetName As String = "Feuil1"   'sheet name of 'Our' workbook
-Const B_SheetName As String = "Feuil1"   'Sheet name of 'Their' workbook
-
-Dim A_arr As Variant, B_arr As Variant
+Const Result_SheetName As String = "Results"
 
 Sub Updatesheets()
-    Dim lRowEnd As Long, lRow As Long, lFound As Long
-    Dim vA_WB As Variant, vB_WB As Variant
-    Dim A_WB As Workbook, B_WB As Workbook
-    Dim A_WS As Worksheet, B_WS As Worksheet
+    Dim rowEnd As Long, row As Long, match As Long
+    Dim A_WB As Workbook, B_WB As Workbook, Result_WB As Workbook
+    Dim A_WS As Worksheet, B_WS As Worksheet, Result_WS As Worksheet
+    
+    Dim A_arr As Variant, B_arr As Variant
 
-    Set A_WB = OpenWB("Select 'Our' excel file")
+    Dim Result_Row As Long = 2 ' Variable for the number of row lines in the worksheet result
 
-    If (A_WB Is Nothing) Then
-        MsgBox "Macro abandoned"
-        Exit Sub
-    End If
+
+    Dim A_Path As String = "FULL PATH TO THE XLSX A"
+    Dim A_colRef As Integer = 3 
+    Dim A_colLnk As Integer = 6   
+    Dim B_Path As String = "FULL PATH TO THE XLSX B"
+    Dim B_colRef As Integer = 3 
+    Dim B_colLnk As Integer = 6
+    Dim A_SheetName As String = "Feuil1"   'sheet name of 'Our' workbook
+    Dim B_SheetName As String = "Feuil1" 
+
+    Set Result_WB = ThisWorkbook
+    Set Result_WS = C_WB.Sheets(Result_SheetName)
+    
+
+    Workbooks.Open Filename:=A_Path
+    Set A_WB = ActiveWorkbook
     Set A_WS = A_WB.Sheets(A_SheetName)
 
-    Set B_WB = OpenWB("Select 'Their' excel file")
-    If B_WB Is Nothing Then
-        A_WB.Close savechanges:=False
-        MsgBox "Macro Abandoned"
-        Exit Sub
-    End If
+    Workbooks.Open Filename:=B_Path
+    Set B_WB = ActiveWorkbook
     Set B_WS = B_WB.Sheets(B_SheetName)
 
-    '-- Store A_WB item numbers into array --
-    lRowEnd = A_WS.Cells(Rows.Count, A_colRef).End(xlUp).Row
-    A_arr = A_WS.Range(Cells(1, A_colRef).Address, _
-        Cells(lRowEnd, A_colRef).Address).Value
+    '-- Store Version A items into array --
+    rowEnd = A_WS.Cells(Rows.Count, A_colRef).End(xlUp).Row
+    A_arr = A_WS.Range(Cells(1, A_colRef).Address, Cells(rowEnd, A_colRef).Address).Value
                         
-    '-- Store B_WB item numbers into array --
-    lRowEnd = B_WS.Cells(Rows.Count, B_colRef).End(xlUp).Row
-    B_arr = B_WS.Range(Cells(1, B_colRef).Address, _
-        Cells(lRowEnd, B_colRef).Address).Value
+    '-- Store Version B items into array --
+    rowEnd = B_WS.Cells(Rows.Count, B_colRef).End(xlUp).Row
+    B_arr = B_WS.Range(Cells(1, B_colRef).Address, Cells(rowEnd, B_colRef).Address).Value
 
-    For lRow = 2 To UBound(B_arr, 1)
-        lFound = 0
+    Result_WS.Cells.ClearContents 'EEEEERRRRRRAAAAAAASSSSSEEEEEEE EVERYTHING!!!!!! MOUHAHAHAHAHAHA
+
+    ' Pause screen update  
+    With Application
+        .ScreenUpdating = False
+        .EnableEvents = False
+    End With
+
+    'Lets put some headers
+    Result_WS.Cells(1,1) = "Workbook_A"
+    Result_WS.Cells(1,2) = "Ref_A"
+    Result_WS.Cells(1,3) = "Lnk_A"
+    Result_WS.Cells(1,4) = "Lnk_B"
+    Result_WS.Cells(1,5) = "Ref_B"
+    Result_WS.Cells(1,6) = "Workbook_B"
+
+    For row = 2 To UBound(A_arr, 1)
+        match = 0
         On Error Resume Next
-        lFound = WorksheetFunction.Match(B_arr(lRow, 1), A_arr, 0)
+        match = WorksheetFunction.Match(B_arr(row, 1), A_arr, 0)
         On Error GoTo 0
         
-        If lFound <> 0 Then
-            ' B_WS.Cells(lRow, B_colLnk).Value = A_WS.Cells(lFound, A_colLnk).Value
-            MsgBox "Ref A " & A_WS.Cells(lFound, A_colRef).Value & "Ref B" & B_WS.Cells(lRow, B_colRef).Value & "Lnk A " & A_WS.Cells(lFound, A_colLnk).Value & "Lnk B" & B_WS.Cells(lRow, B_colLnk).Value
+        Result_WS.Cells(Result_Row,1) = "Prout A"
+        Result_WS.Cells(Result_Row,2) = A_WS.Cells(match, A_colRef).Value
+        Result_WS.Cells(Result_Row,3) = A_WS.Cells(match, A_colLnk).Value
+
+        If match <> 0 Then
+            ' B_WS.Cells(row, B_colLnk).Value = A_WS.Cells(match, A_colLnk).Value
+            ' MsgBox "Ref A " & A_WS.Cells(match, A_colRef).Value & "Ref B" &  & "Lnk A " & A_WS.Cells(match, A_colLnk).Value & "Lnk B" & B_WS.Cells(row, B_colLnk).Value
+            Result_WS.Cells(Result_Row,4) = B_WS.Cells(row, B_colLnk).Value
+            Result_WS.Cells(Result_Row,5) = B_WS.Cells(row, B_colRef).Value
+
         End If
 
-    Next lRow
+        Result_WS.Cells(Result_Row,6) = "Prout B"
+
+        Result_Row = Result_Row + 1
+
+    Next row
+
+    With Application
+        .ScreenUpdating = True
+        .EnableEvents = True
+    End With
+
+
 End Sub
 
 Private Function OpenWB(ByVal Title As String) As Workbook
@@ -68,6 +102,15 @@ Private Function OpenWB(ByVal Title As String) As Workbook
     Set OpenWB = ActiveWorkbook
 End Function
 
-                    '  /\_/\
-                    ' ( o.o )
-                    '  > ^ <    
+'                       /^--^\     /^--^\     /^--^\
+'                       \____/     \____/     \____/
+'                      /      \   /      \   /      \
+'                     |        | |        | |        |
+'                      \__  __/   \__  __/   \__  __/
+' |^|^|^|^|^|^|^|^|^|^|^|^\ \^|^|^|^/ /^|^|^|^|^\ \^|^|^|^|^|^|^|^|^|^|^|^|
+' | | | | | | | | | | | | |\ \| | |/ /| | | | | | \ \ | | | | | | | | | | |
+' | | | | | | | | | | | | / / | | |\ \| | | | | |/ /| | | | | | | | | | | |
+' | | | | | | | | | | | | \/| | | | \/| | | | | |\/ | | | | | | | | | | | |
+' #########################################################################
+' | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
+' | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |
